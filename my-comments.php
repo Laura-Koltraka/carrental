9a2e4b6fdc3c566ce879c067386e75785afdfe7e
Laurakoltraka <96212987+Laurakoltraka@users.noreply.github.com>
2023-04-09 16:54:49 +0200

    Logo

--- my-comments.php
+++ my-comments.php
@@ -13,62 +13,50 @@ else{
 <html lang="en">
 <head>
 
-<title>Car Rental Portal | My Testimonials </title>
-<!--Bootstrap -->
+<title>Rental.al | My Comments </title>
+
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-<!--Custome Style -->
+
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-<!--OWL Carousel slider-->
+
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
 <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-<!--slick-slider -->
+
 <link href="assets/css/slick.css" rel="stylesheet">
-<!--bootstrap-slider -->
+
 <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-<!--FontAwesome Font Style -->
+
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
 
-<!-- SWITCHER -->
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
-        
-<!-- Fav and touch icons -->
+
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
 <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
-<!-- Google-Font-->
+
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 </head>
 <body>
-<?php include('includes/colorswitcher.php');?>
-<!-- /Switcher -->  
-        
-<!--Header-->
+
+
 <?php include('includes/header.php');?>
-<!--Page Header-->
+
 <section class="page-header profile_page">
   <div class="container">
     <div class="page-header_wrap">
       <div class="page-heading">
-        <h1>My Testimonials</h1>
+        <h1>My Comments</h1>
       </div>
       <ul class="coustom-breadcrumb">
         <li><a href="#">Home</a></li>
-        <li>My Testimonials</li>
+        <li>My Comments</li>
       </ul>
     </div>
   </div>
-  <!-- Dark Overlay-->
+ 
   <div class="dark-overlay"></div>
 </section>
-<!-- /Page Header--> 
 
 <?php 
 $useremail=$_SESSION['login'];
@@ -103,7 +91,7 @@ foreach($results as $result)
 
 
         <div class="profile_wrap">
-          <h5 class="uppercase underline">My Testimonials </h5>
+          <h5 class="uppercase underline">My comments </h5>
           <div class="my_vehicles_list">
             <ul class="vehicle_listing">
 <?php 
@@ -146,24 +134,18 @@ foreach($results as $result)
     </div>
   </div>
 </section>
-<!--/my-vehicles--> 
 
-<<!--Footer -->
 <?php include('includes/footer.php');?>
-<!-- /Footer--> 
 
-<!--Back to top-->
 <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
 
-<!-- Scripts --> 
+
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script> 
 <script src="assets/js/interface.js"></script> 
-<!--Switcher-->
-<script src="assets/switcher/js/switcher.js"></script>
-<!--bootstrap-slider-JS--> 
+
 <script src="assets/js/bootstrap-slider.min.js"></script> 
-<!--Slider-JS--> 
+
 <script src="assets/js/slick.min.js"></script> 
 <script src="assets/js/owl.carousel.min.js"></script>
 
--- update-password.php
+++ update-password.php
@@ -38,37 +38,31 @@ $error="Your current password is wrong";
 <html lang="en">
 <head>
 
-<title>Car Rental Portal - Update Password</title>
-<!--Bootstrap -->
+<title>Rental.al | Update Password</title>
+
+
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-<!--Custome Style -->
+
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-<!--OWL Carousel slider-->
+
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
 <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-<!--slick-slider -->
+
 <link href="assets/css/slick.css" rel="stylesheet">
-<!--bootstrap-slider -->
+
 <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-<!--FontAwesome Font Style -->
+
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
 
-<!-- SWITCHER -->
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
-        
-<!-- Fav and touch icons -->
+
+
+
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
 <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
-<!-- Google-Font-->
+
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 <script type="text/javascript">
 function valid()
@@ -103,14 +97,10 @@ return true;
 </head>
 <body>
 
-<!-- Start Switcher -->
-<?php include('includes/colorswitcher.php');?>
-<!-- /Switcher -->  
-        
-<!--Header-->
+
 <?php include('includes/header.php');?>
-<!-- /Header --> 
-<!--Page Header-->
+
+
 <section class="page-header profile_page">
   <div class="container">
     <div class="page-header_wrap">
@@ -123,10 +113,11 @@ return true;
       </ul>
     </div>
   </div>
-  <!-- Dark Overlay-->
+  
   <div class="dark-overlay"></div>
 </section>
-<!-- /Page Header--> 
+
+
 
 <?php 
 $useremail=$_SESSION['login'];
@@ -143,7 +134,7 @@ foreach($results as $result)
 <section class="user_profile inner_pages">
   <div class="container">
     <div class="user_profile_info gray-bg padding_4x4_40">
-      <div class="upload_user_logo"> <img src="assets/images/dealer-logo.jpg" alt="image">
+      <div class="upload_user_logo"> <img src="assets/images/dealer-rental_logo.jpg" alt="image">
       </div>
 
       <div class="dealer_info">
@@ -187,38 +178,30 @@ foreach($results as $result)
     </div>
   </div>
 </section>
-<!--/Profile-setting--> 
 
-<<!--Footer -->
+
 <?php include('includes/footer.php');?>
-<!-- /Footer--> 
 
-<!--Back to top-->
+
 <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
-<!--/Back to top--> 
 
-<!--Login-Form -->
+
 <?php include('includes/login.php');?>
-<!--/Login-Form --> 
 
-<!--Register-Form -->
+
 <?php include('includes/registration.php');?>
 
-<!--/Register-Form --> 
 
-<!--Forgot-password-Form -->
+
 <?php include('includes/forgotpassword.php');?>
-<!--/Forgot-password-Form --> 
 
-<!-- Scripts --> 
+
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script> 
 <script src="assets/js/interface.js"></script> 
-<!--Switcher-->
-<script src="assets/switcher/js/switcher.js"></script>
-<!--bootstrap-slider-JS--> 
+
 <script src="assets/js/bootstrap-slider.min.js"></script> 
-<!--Slider-JS--> 
+
 <script src="assets/js/slick.min.js"></script> 
 <script src="assets/js/owl.carousel.min.js"></script>
 
