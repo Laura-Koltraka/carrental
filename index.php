365f0f848fcb97ab67586d166712e6bb53b19d40
Laurakoltraka <96212987+Laurakoltraka@users.noreply.github.com>
2023-04-09 16:19:12 +0200

    .

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ error_reporting(0);
 <html lang="en">
 <head>
 
-<title>Car Rental Portal</title>
-<!--Bootstrap -->
+<title>Rental.al | Luxury Car Rental</title>
+
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
@@ -18,13 +18,7 @@ error_reporting(0);
 <link href="assets/css/slick.css" rel="stylesheet">
 <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
 <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+	
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
@@ -34,15 +28,11 @@ error_reporting(0);
 </head>
 <body>
 
-<!-- Start Switcher -->
-<?php include('includes/colorswitcher.php');?>
-<!-- /Switcher -->  
-        
-<!--Header-->
-<?php include('includes/header.php');?>
-<!-- /Header --> 
 
-<!-- Banners -->
+
+<?php include('includes/header.php');?>
+
+
 <section id="banner" class="banner-section">
   <div class="container">
     <div class="div_zindex">
@@ -57,25 +47,23 @@ error_reporting(0);
     </div>
   </div>
 </section>
-<!-- /Banners --> 
 
-
-<!-- Resent Cat-->
 <section class="section-padding gray-bg">
   <div class="container">
     <div class="section-header text-center">
       <h2>Find the Best <span>CarForYou</span></h2>
-      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+      <p>There are many variations of cars.</p>
     </div>
     <div class="row"> 
       
-      <!-- Nav tabs -->
+     
       <div class="recent-tab">
         <ul class="nav nav-tabs" role="tablist">
           <li role="presentation" class="active"><a href="#resentnewcar" role="tab" data-toggle="tab">New Car</a></li>
         </ul>
       </div>
-      <!-- Recently Listed New Cars -->
+      
+      
       <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="resentnewcar">
 
@@ -114,16 +102,14 @@ foreach($results as $result)
     </div>
   </div>
 </section>
-<!-- /Resent Cat --> 
 
-<!-- Fun Facts-->
 <section class="fun-facts-section">
   <div class="container div_zindex">
     <div class="row">
       <div class="col-lg-3 col-xs-6 col-sm-3">
         <div class="fun-facts-m">
           <div class="cell">
-            <h2><i class="fa fa-calendar" aria-hidden="true"></i>40+</h2>
+            <h2><i class="fa fa-calendar" aria-hidden="true"></i>10+</h2>
             <p>Years In Business</p>
           </div>
         </div>
@@ -131,7 +117,7 @@ foreach($results as $result)
       <div class="col-lg-3 col-xs-6 col-sm-3">
         <div class="fun-facts-m">
           <div class="cell">
-            <h2><i class="fa fa-car" aria-hidden="true"></i>1200+</h2>
+            <h2><i class="fa fa-car" aria-hidden="true"></i>120+</h2>
             <p>New Cars For Sale</p>
           </div>
         </div>
@@ -139,7 +125,7 @@ foreach($results as $result)
       <div class="col-lg-3 col-xs-6 col-sm-3">
         <div class="fun-facts-m">
           <div class="cell">
-            <h2><i class="fa fa-car" aria-hidden="true"></i>1000+</h2>
+            <h2><i class="fa fa-car" aria-hidden="true"></i>100+</h2>
             <p>Used Cars For Sale</p>
           </div>
         </div>
@@ -154,13 +140,11 @@ foreach($results as $result)
       </div>
     </div>
   </div>
-  <!-- Dark Overlay-->
+  
   <div class="dark-overlay"></div>
 </section>
-<!-- /Fun Facts--> 
 
 
-<!--Testimonial -->
 <section class="section-padding testimonial-section parallex-bg">
   <div class="container div_zindex">
     <div class="section-header white-text text-center">
@@ -198,46 +182,38 @@ foreach($results as $result)
       </div>
     </div>
   </div>
-  <!-- Dark Overlay-->
+  
   <div class="dark-overlay"></div>
 </section>
-<!-- /Testimonial--> 
 
 
-<!--Footer -->
 <?php include('includes/footer.php');?>
-<!-- /Footer--> 
 
-<!--Back to top-->
+
 <div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
-<!--/Back to top--> 
 
-<!--Login-Form -->
+
 <?php include('includes/login.php');?>
-<!--/Login-Form --> 
 
-<!--Register-Form -->
+
 <?php include('includes/registration.php');?>
 
-<!--/Register-Form --> 
 
-<!--Forgot-password-Form -->
+
 <?php include('includes/forgotpassword.php');?>
-<!--/Forgot-password-Form --> 
 
-<!-- Scripts --> 
+
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script> 
 <script src="assets/js/interface.js"></script> 
-<!--Switcher-->
-<script src="assets/switcher/js/switcher.js"></script>
-<!--bootstrap-slider-JS--> 
+
+
 <script src="assets/js/bootstrap-slider.min.js"></script> 
-<!--Slider-JS--> 
+
 <script src="assets/js/slick.min.js"></script> 
 <script src="assets/js/owl.carousel.min.js"></script>
 
 </body>
 
-<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:22:11 GMT -->
+
 </html>
